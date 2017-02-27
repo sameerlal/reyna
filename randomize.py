2d71358f82c9e80b9edc7accd52fe3fdc0b96af7
Sameer Lal <sj_lal@hotmail.com>
2017-02-27 14:31:49 -0500

    footprint

---
+++ randomize.py
@@ -0,0 +1,22 @@
+from math import *
+import csv
+
+
+
+
+with open('input.csv', 'wb') as csvfile:
+	spamwriter = csv.writer(csvfile, delimiter = ',',
+
+
+
+#  read file
+
+
+#randomize
+
+
+#output
+
+
+
+
