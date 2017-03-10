37f6da47ef69551181a47d06135a5baeec4722b5
Sameer Lal <sj_lal@hotmail.com>
2017-03-10 15:25:25 -0500

    updated

--- randomize.py
+++ randomize.py
@@ -1,21 +1,63 @@
 from math import *
 import csv
+import argparse
 
 
 
 
-with open('input.csv', 'wb') as csvfile:
-	spamwriter = csv.writer(csvfile, delimiter = ',',
+parser = argparse.ArgumentParser(description='Randomize subject to certain conditions for Hot-Cold study.')
+parser.add_argument('-f','--file', help='CSV file containing questions and attributes.  File should be in same directory.', required=True)
+args = vars(parser.parse_args())
+
+
+f = open(args['file'], 'r')
+
+print f.read()
 
 
 
-#  read file
+
+def parse_file():
 
 
-#randomize
+	return
 
 
-#output
+
+def file_reader_crypt():
+	# open file
+
+	# parse select data
+
+	# encode
+
+	#return array
+	return
+
+
+
+def shuffle():
+	# shuffle into new array
+
+	# return shuffled array
+	return
+
+
+
+def write_out():
+	# write output file
+
+
+	return
+
+
+
+
+
+
+
+
+
 
 
 
