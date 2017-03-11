45df3557373ea17be828fe50b9eb77d7595d57c7
Sameer Lal <sj_lal@hotmail.com>
2017-03-10 19:02:56 -0500

    added csv update

--- randomize.py
+++ randomize.py
@@ -4,15 +4,37 @@ import argparse
 
 
 
-
+#	Parse Arguments
 parser = argparse.ArgumentParser(description='Randomize subject to certain conditions for Hot-Cold study.')
 parser.add_argument('-f','--file', help='CSV file containing questions and attributes.  File should be in same directory.', required=True)
+parser.add_argument('-s', '--seed', default = "-1", help = 'Participant ID number will be used as a seed for randomization', required = False)
 args = vars(parser.parse_args())
 
+ 
+#print "seed" + args['seed']
+#print "file" + args['file']
 
-f = open(args['file'], 'r')
+dataquest = []	#	Contains questions in a list of lists 
 
-print f.read()
+with open(args['file'], 'rb') as csvfile:
+	readcsv = csv.reader(csvfile, delimiter = ',')
+	for row in readcsv:
+		dataquest.append(row)
+
+x =  dataquest[2]
+for i in range(len(x)):
+	print x[i]
+
+
+def create_dictionary(lolists):
+	'''Idea is to create a dictionary that will link a question's ID to
+	its attributes.  That is, the key:value pair will be questionID:attributes
+	'''
+	# Use K-th entry to extract the question ID
+
+	#Create an array that stores NEEDED attributes.  Carefuly write this in docs
+
+
 
 
 
