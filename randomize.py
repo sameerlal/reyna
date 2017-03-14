f2de76490c7247fafb1b5b0ba619e3b756df90d9
Sameer Lal <sj_lal@hotmail.com>
2017-03-14 16:22:44 -0400

    PROMISING

--- randomize.py
+++ randomize.py
@@ -2,6 +2,7 @@ from math import *
 import csv
 import argparse
 import random
+import itertools 
 
 
 
@@ -12,8 +13,7 @@ parser.add_argument('-s', '--seed', default = "-1", help = 'Participant ID numbe
 args = vars(parser.parse_args())
 
  
-#print "seed" + args['seed']
-#print "file" + args['file']
+
 
 dataquest = []	#	Contains questions in a list of lists 
 
@@ -21,62 +21,67 @@ with open(args['file'], 'rb') as csvfile:
 	readcsv = csv.reader(csvfile, delimiter = ',')
 	for row in readcsv:
 		dataquest.append(row)
-y = dataquest[2]
-x =  dataquest[3]
-for i in range(len(x)):
-	print x[i]
-	print y[i]
 
+  
 
 def create_dictionary(lolists):
 	'''Idea is to create a dictionary that will link a question's ID to
-	its attributes.  That is, the key:value pair will be questionID:attributes
+	its attributes.  That is, the key:value pair will be questionID:row
+	Map listoflists[i][8] --> listoflists[i]
 	'''
-	# TODO
-	# Use K-th entry to extract the question ID
+	dic = {}
+	for i in range(1,len(lolists)):
+		dic[str(lolists[i][8])] = lolists[i]
+	return dic
 
-	#Create an array that stores NEEDED attributes.  Carefuly write this in docs
-
-def master_shuffle():
-	'''merges two 108 quesiton randomized blocks.'''
-
-
-
+# Create mapping
+data_dic = create_dictionary(dataquest)
+ 
 
 
-def shuffle_scale_block():
+
+def shuffle_small_block():
 	''''Creates a scale-type-block shuffling category, stim size, plate size
 		There are two blocks of this.  18Q
 		8-bit:  pqqrrXXX
 		p =[0,1] 		 = category
 		q = [10,01,00]	 = stimsize
-		r = [10,01,11]	 = platesize 
+		r = [10,01,00]	 = platesize 
 
 		RETURNS A LIST OF 18 QUESTIONS
 	'''
-	
-	list_q = []
-	for i in range(18):
-		list_q.append(shuffle_segment())
 
-	return list_q
+	out = []
 
-def shuffle_segment():
-	'''Creates one randomized shufled 3-part question
-		8-bit:  pqqrrXXX
-		p =[0,1] 		 = category
-		q = [10,01,00]	 = stimsize
-		r = [10,01,11]	 = platesize 
+	list_p = ["1", "0"]
+	list_q = ["10", "01", "00"]
+	list_r = ["10", "01", "00"]
+	s = [list_p, list_q, list_r]
+	list_pqr = list(itertools.product(*s))
+	random.shuffle(list_pqr)
+	for item in list_pqr:
+		st = ""
+		for i in range(3):
+			st = st + "" + str(item[i])
+		out.append(st)
+	return out
+
+def shuffle_big_block():
+	'''Creates two blocks of 18Q each, totalling 36 questions.
+	Incorporates Scale Type: 0 = Verbatim, 1= gist
 	'''
-
-	#Randomize category
-	p = random.choice(['0','1'])
-	#Randomize Stimsize
-	q = random.choice(['10','01','00'])
-	#Randomize Plate Size
-	r = random.choice(['10','01','00'])
 	
-	return p+q+r
+	out_block = []
+	first_piv = random.randrange(0,2)
+	list_one = shuffle_small_block()
+	list_two = shuffle_small_block()
+
+	for ele in list_one:
+		out_block.append(ele + str(first_piv))
+	for ele in list_two:
+		out_block.append( ele + str(1-first_piv))
+	return out_block #36 Questions
+
 
 
 
@@ -84,34 +89,65 @@ def shuffle_combine(a,b,c):
 	'''Create 108 randomized 8-bit numbers according to specifications in argument
 		Both PM and Wanting will have the same None-Verbatim-Gist (Label type) order
 		however within each 18 question block, the questions will be randomized.
+		[10=gist;    01=verb;   00=none]  
+		Already in order a,b,c = label type
+		Inputs:  a,b,c order of gist/verb/none
+		Return:  36*3 = 108 Q
+
+		INcorporates Label type at end
 	'''
-	# SCALE TYPE:  [0 = verbatim, 1= gist]
-	# For a, shuffle scale type and make two blocks
-	aPivot = random.randint(0,1)
+		# create 3 36-question blocks and postpend a,b,c in that order
+	block_a = shuffle_big_block()
+	block_b = shuffle_big_block()
+	block_c = shuffle_big_block()
+
+	out_block = []  #list containing all 108 Q
+
+	for ele in block_a:
+		out_block.append(ele + "" + str(a))
+	for ele in block_b:
+		out_block.append(ele + "" + str(b))
+	for ele in block_c:
+		out_block.append(ele + "" + str(c))
+	return out_block
+
+
+
+def master_shuffle():
+	'''merges two 108 quesiton randomized blocks.'''
+	#determine gist/verbatim/none order
+	choices = ["None", "Verbatim", "Gist"]
+	pointer_c = ["00", "01", "10"]
+	random.shuffle(pointer_c)
+	#create shuffle_combine blocks
+	shuffled_108_1 = shuffle_combine(pointer_c[0], pointer_c[1], pointer_c[2])
+	shuffled_108_2 = shuffle_combine(pointer_c[0], pointer_c[1], pointer_c[2])
+	foo = []
+	foo.extend(shuffled_108_1)
+	foo.extend(shuffled_108_2)	
+	return foo
+
+
+
+def write_out(dictionary, order):
 	
+	#print dictionary
+	#print order
+	outfilename = "thisistheoutputfile" + str(args['seed']) + ".csv"
+	with open(outfilename, 'wb') as csvfile:
+		outwriter = csv.writer(csvfile, delimiter = ',')
+		for value in order:
+			print value
+			print dictionary[str(value)]
+			outwriter.writerow(dictionary[str(value)])
+	return "SUCCESS"
+		
 
-	# merge and return
-	
-	return list_a.append(list_b).append(list_c)
+finish = "not successful"
 
-addd = shuffle_combine(1,2,2)
-print addd
-
-
-
-def write_out():
-	# write output file
-
-
-	return
-
-
-
-
-
-
-
-
+order_shuffle =  master_shuffle()
+finish = write_out(data_dic, order_shuffle)
+print finish
 
 
 
